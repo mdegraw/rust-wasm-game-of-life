ac15a29233ceb4b4444e0af32d012b2b2ff16b82
mdegraw <michael.degraw@protonmail.com>
2021-11-07 02:44:21 -0700

    add gh-pages

--- packages/web/gh-actions.cjs
+++ packages/web/gh-actions.cjs
@@ -4,7 +4,7 @@ ghpages.publish(
     'public', // path to public directory
     {
         branch: 'gh-pages',
-        repo: 'https://github.com/mdegraw/yourproject.git',
+        repo: 'https://github.com/mdegraw/rust-wasm-game-of-life.git',
         user: {
             name: 'mdegraw',
             email: 'michael.degraw@protonmail.com'
