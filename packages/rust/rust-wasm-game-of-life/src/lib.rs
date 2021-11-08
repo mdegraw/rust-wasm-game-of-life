19e5a0e7a75b70bdcbaed9dc479b3908f4ecb4f9
mdegraw <michael.degraw@protonmail.com>
2021-11-07 20:49:48 -0700

    add test

--- packages/rust/rust-wasm-game-of-life/src/lib.rs
+++ packages/rust/rust-wasm-game-of-life/src/lib.rs
@@ -58,6 +58,17 @@ impl Universe {
 
         count
     }
+
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
+    }
 }
 
 /// Public methods, exported to JavaScript.
@@ -160,6 +171,16 @@ impl Universe {
         let idx = self.get_index(row, column);
 
         self.cells[idx].toggle();
+    }
+
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+    }
+
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
     }
 }
 
